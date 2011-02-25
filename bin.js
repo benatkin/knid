44a061e8305d00ae1f68a3684273f57c5a41dcb9
Ben Atkin <ben@benatkin.com>
2011-02-24 19:25:56 -0700

    fixed showing errors with ddoc; reverse chrono

--- bin.js
+++ bin.js
@@ -1,29 +1,26 @@
 #!/usr/bin/env node
 var Knid = require('./knid.js').Knid
   , argv = require('optimist')
-    .usage(["knid p http://example.com/ comment can span multiple arguments"
-          , "  creates a configuration file in ~/.knid"
-          , "knid config"
-          , "  creates a configuration file in ~/.knid"
-          , "knid ddoc"
-          , "  creates or updates the ddoc"].join("\n"))
-    .demand(1)
-    .check(function (argv) {
-      var command = argv._[0]
-      if (['p', 'config', 'ddoc'].indexOf(command) < 0) {
-        throw 'invalid command: ' + argv._[0]
-      }
-      if (command === 'p') {
-        if (argv._.length < 3) throw 'p takes 2+ arguments'
-      } else if (command === 'config') {
-        if (argv._.length != 1) throw 'config takes no arguments'
-      } else if (command === 'ddoc') {
-        if (argv._.length != 1) throw 'ddoc takes no arguments'
-      }
-    })
-    .argv
+           .usage(["knid post http://example.com/ comment can span multiple arguments"
+                 , "  posts a link"
+                 , "knid config"
+                 , "  creates a configuration file in ~/.knid"
+                 , "knid ddoc"
+                 , "  creates or updates the ddoc"].join("\n"))
+           .demand(1)
+           .check(function (argv) {
+             var command = argv._[0]
+               , args    = argv._.slice(1)
+             if (['post', 'config', 'ddoc'].indexOf(command) < 0)
+               throw 'invalid command: ' + command
+             if (command === 'config')
+               if (argv._.length != 1) throw command + ' takes 0 arguments'
+             else if (command === 'ddoc')
+               if (argv._.length != 1) throw 'ddoc takes no arguments'
+           })
+           .argv
   , command = argv._[0]
-  , Seq = require('seq')
+  , args    = argv._.slice(1)
 
 var knid = new Knid()
 if (command === 'config') {
@@ -31,7 +28,7 @@ if (command === 'config') {
 } else {
   knid.loadConfig(function() {
     if (command === 'post') {
-      knid.post(argv._.slice(1))
+      knid.post(args, argv)
     } else if (command === 'ddoc') {
       knid.ddoc()
     }
--- knid.js
+++ knid.js
@@ -4,6 +4,78 @@ var cradle = require('cradle')
   , path = require('path')
   , url = require('url')
 
+var designDoc = {
+  views: {
+    recent: {
+      map: function(doc) {
+        if (doc.created_at) {
+          emit(doc.created_at, doc)
+        }
+      }
+    }
+  },
+  lists: {
+    'recent-stream': function(head, req) {
+      /*
+       flatstache.js - Logic-less, section-less templates in JavaScript. Expands simple (flat) Mustache templates.
+       (c) 2011 Nathan Vander Wilt and Aaron McCall. MIT license.
+      */
+      var Flatstache = (function(){
+          var _re3 = /{{{\s*(\w+)\s*}}}/g,
+              _re2 = /{{\s*(\w+)\s*}}/g,
+              _re1 = /[&\"'<>\\]/g,  // "
+              _escape_map = {"&": "&amp;", "\\": "&#92;", "\"": "&quot;", "'": "&#39;", "<": "&lt;", ">": "&gt;"},
+              _escapeHTML = function(s) {
+                  return s.replace(_re1, function(c) { return _escape_map[c]; });
+              },
+              pub = {};
+          pub.to_html = function (template, data) {
+              return template
+                  .replace(_re3, function (m, key) { return data[key] || ""; })
+                  .replace(_re2, function (m, key) { return _escapeHTML(data[key] || ""); });
+          };
+          return pub;
+      })();
+    
+      var ddoc = this;
+
+      provides('html', function() {
+        posts = []
+        while (row = getRow()) {
+          posts.push(Flatstache.to_html(ddoc.templates.link, row.value));
+        }
+        send(Flatstache.to_html(ddoc.templates.page, {posts: posts.join("\n")}))
+      })
+    }
+  },
+  rewrites: [
+    {
+      from: '',
+      to: '_list/recent-stream/recent',
+      method: 'GET',
+      query: {
+        descending: true
+      }
+    }
+  ],
+  templates: {
+    page: ["<!DOCTYPE html>"
+         , "<html lang=\"en\">"
+         , "<head>"
+         , "  <meta charset=\"utf-8\" />"
+         , "  <title>stream</title>"
+         , "</head>"
+         , "<body id=\"home\">"
+         , "  <h1>stream</h1>"
+         , "  {{{ posts }}}"
+         , "</body>"
+         , "</html>\n"].join("\n"),
+    link: ["<div class=\"link\">"
+         , "  <a href=\"{{ url }}\">{{ text }}</a>"
+         , "</div>"].join("\n")
+  }
+};
+
 var Knid = exports.Knid = function() {
   this.configDir = path.join(process.env.HOME, '.knid')
   this.configFile = path.join(this.configDir, 'config.json')
@@ -13,13 +85,13 @@ Knid.prototype = {
   defaultConfig: {
     db: 'http://localhost:5984/knid'
   },
-  post: function(parg) {
+  post: function(args, argv) {
     var that = this
     Seq()
     .seq(function() {
       that.db.save({
-        url: parg[0],
-        text: parg.slice(1).join(' '),
+        url: args[0],
+        text: args.slice(1).join(' '),
         created_at: new Date()
       }, this)
     })
@@ -81,74 +153,13 @@ Knid.prototype = {
     })
   },
   ddoc: function() {
-    this.db.save('_design/knid', {
-      views: {
-        recent: {
-          map: function(doc) {
-            if (doc.created_at) {
-              emit(doc.created_at, doc)
-            }
-          }
-        }
-      },
-      lists: {
-        'recent-stream': function(head, req) {
-          /*
-           flatstache.js - Logic-less, section-less templates in JavaScript. Expands simple (flat) Mustache templates.
-           (c) 2011 Nathan Vander Wilt and Aaron McCall. MIT license.
-          */
-          var Flatstache = (function(){
-              var _re3 = /{{{\s*(\w+)\s*}}}/g,
-                  _re2 = /{{\s*(\w+)\s*}}/g,
-                  _re1 = /[&\"'<>\\]/g,  // "
-                  _escape_map = {"&": "&amp;", "\\": "&#92;", "\"": "&quot;", "'": "&#39;", "<": "&lt;", ">": "&gt;"},
-                  _escapeHTML = function(s) {
-                      return s.replace(_re1, function(c) { return _escape_map[c]; });
-                  },
-                  pub = {};
-              pub.to_html = function (template, data) {
-                  return template
-                      .replace(_re3, function (m, key) { return data[key] || ""; })
-                      .replace(_re2, function (m, key) { return _escapeHTML(data[key] || ""); });
-              };
-              return pub;
-          })();
-        
-          var ddoc = this;
-
-          provides('html', function() {
-            posts = []
-            while (row = getRow()) {
-              posts.push(Flatstache.to_html(ddoc.templates.link, row.value));
-            }
-            send(Flatstache.to_html(ddoc.templates.page, {posts: posts.join("\n")}))
-          })
-        }
-      },
-      rewrites: [
-        {
-          from: '',
-          to: '_list/recent-stream/recent',
-          method: 'GET',
-          query: {}
-        }
-      ],
-      templates: {
-        page: ["<!DOCTYPE html>"
-             , "<html lang=\"en\">"
-             , "<head>"
-             , "  <meta charset=\"utf-8\" />"
-             , "  <title>knid stream</title>"
-             , "</head>"
-             , "<body id=\"home\">"
-             , "  <h1>knid stream</h1>"
-             , "  {{{ posts }}}"
-             , "</body>"
-             , "</html>\n"].join("\n"),
-        link: ["<div class=\"link\">"
-             , "  <a href=\"{{ url }}\">{{ text }}</a>"
-             , "</div>"].join("\n")
-      }
+    var that = this;
+    Seq()
+    .seq(function() {
+      that.db.save('_design/knid', designDoc, this);
+    })
+    .seq(function() {
+      console.log('updated design doc!');
     })
   }
 }
